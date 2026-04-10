89bb05b237a271fda69a19f0a35201ff77470101
AnneBeyer <anne.beyer@mailbox.org>
2026-04-10 15:01:28 +0200

    final test fix (hopefully)

--- sklearn/inspection/_plot/tests/test_boundary_decision_display.py
+++ sklearn/inspection/_plot/tests/test_boundary_decision_display.py
@@ -791,14 +791,17 @@ def test_multiclass_levels(pyplot, y, response_method, plot_method):
         expected_colors = pyplot.get_cmap("gist_rainbow")(np.linspace(0, 1, 6))
         import matplotlib as mpl
 
-        # In matplotlib 3.10.0, the API was changed to produce only one collection per
-        # plot, whereas before, a collection was created for each level separately.
+        # disp.surface_ is QuadContourSet. In matplotlib 3.10.0, the API for
+        # QuadContourSet was changed to produce only one collection per plot and
+        # `.collections` was deprecated, whereas before, a collection was created for
+        # each level separately.
         if parse_version(mpl.__version__) >= parse_version("3.10.0"):
-            surface_colors = disp.ax_.collections[0].get_facecolor()
+            surface_colors = disp.surface_.get_facecolor()
         else:
-            surface_colors = np.array(
-                [collection.get_facecolor()[0] for collection in disp.ax_.collections]
-            )
+            surface_colors = [
+                collection.get_facecolor()[0]
+                for collection in disp.surface_.collections
+            ]
         assert_allclose(expected_colors, surface_colors)
 
 
@@ -807,19 +810,37 @@ def test_multiclass_levels(pyplot, y, response_method, plot_method):
 )
 @pytest.mark.parametrize("plot_method", ["contourf", "contour", "pcolormesh"])
 def test_zorder(pyplot, response_method, plot_method):
-    """Check that decision boundaries are plottet in the background."""
+    """Check that decision boundaries are plotted in the background."""
     X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1], [2, 2], [3, 2]])
     y = np.arange(6)
     clf = LogisticRegression().fit(X, y)
     disp = DecisionBoundaryDisplay.from_estimator(
         clf, X, response_method=response_method, plot_method=plot_method
     )
+    import matplotlib as mpl
 
-    if isinstance(disp.surface_, list):
-        for surface in disp.surface_:
-            assert surface.get_zorder() == -1
+    # disp.surface_ is QuadContourSet or QuadMesh (for "pcolormesh"). In matplotlib
+    # 3.10.0, the API for QuadContourSet was changed to produce only one collection
+    # per plot (as it was and is the case for QuadMesh) and `.collections` was
+    # deprecated, whereas before, a collection was created for each level
+    # separately.
+    if (
+        parse_version(mpl.__version__) >= parse_version("3.10.0")
+        or plot_method == "pcolormesh"
+    ):
+        if isinstance(disp.surface_, list):
+            for surface in disp.surface_:
+                assert surface.get_zorder() == -1
+        else:
+            assert disp.surface_.get_zorder() == -1
     else:
-        assert disp.surface_.get_zorder() == -1
+        if isinstance(disp.surface_, list):
+            for surface in disp.surface_:
+                for collection in surface.collections:
+                    assert collection.get_zorder() == -1
+        else:
+            for collection in disp.surface_.collections:
+                assert collection.get_zorder() == -1
 
 
 # estimator classes for non-regression test cases for issue #33194
